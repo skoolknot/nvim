30c46c920ae7ebfd293640a0d8abcb23fa3824ea
Azfar Tomi <azfar@moredoor.com.my>
2023-08-29 22:20:40 +0800

    Enabled mouse mode

--- init.lua
+++ init.lua
@@ -92,6 +92,8 @@ vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 
+vim.opt.mouse = 'a'
+
 -- remaps
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
